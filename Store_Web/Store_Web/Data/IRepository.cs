5694f5f47b613ca2d16fef7cb586591bbd4dccfb
XtareS <xtare16.soares@gmail.com>
2021-01-05 11:19:48 +0000

    REpository creation_

---
+++ Store_Web/Store_Web/Data/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using Store_Web.Data.Enteties;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Store_Web.Data
+{
+    public interface IRepository
+    {
+        void AddProduct(Product product);
+        Product GetProduct(int id);
+        IEnumerable<Product> GetProducts();
+        bool ProductExists(int id);
+        void RemoveProduct(Product product);
+        Task<bool> SaveAllAsync();
+        void UpdateProduct(Product product);
+    }
+}

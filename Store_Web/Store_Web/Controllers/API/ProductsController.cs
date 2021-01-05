f90678d2d36e8aaad2661e9512ec2d368e63667b
XtareS <xtare16.soares@gmail.com>
2021-01-05 16:41:31 +0000

    API-Implmt + 14 Clip

---
+++ Store_Web/Store_Web/Controllers/API/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Store_Web.Data;
+
+namespace Store_Web.Controllers.API
+{
+    [Route("api/[Controller]")]
+    public class ProductsController : Controller
+    {
+
+        private readonly IProductRepository productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(this.productRepository.GetAll());
+        }
+    }
+}

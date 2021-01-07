2185097aaa3f275cfbf4c615a123c167096216fa
XtareS <xtare16.soares@gmail.com>
2021-01-07 08:48:33 +0000

    CascadeOverride+BugPrice Fix

--- Store_Web/Store_Web/Data/DataContext.cs
+++ Store_Web/Store_Web/Data/DataContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Store_Web.Data.Enteties;
+using System.Linq;
 
 namespace Store_Web.Data
 {
@@ -17,6 +18,27 @@ namespace Store_Web.Data
 
         }
 
+        protected override void OnModelCreating(ModelBuilder modelbuilder)
+        {
+            /* correcção de bug nos Prices */
+            modelbuilder.Entity<Product>()
+                 .Property(p => p.Price)
+                 .HasColumnType("decimal(18,2)");
+
+            /* Override de cascade deletting metethed */
+            var cascadeFKS = modelbuilder.Model
+                .GetEntityTypes()
+                .SelectMany(t => t.GetForeignKeys())
+                .Where(fk => !fk.IsOwnership && fk.DeleteBehavior == DeleteBehavior.Cascade);
+
+
+            foreach(var fk in cascadeFKS)
+            {
+                fk.DeleteBehavior=DeleteBehavior.Restrict;
+            }
+
+            base.OnModelCreating(modelbuilder);
+        }
 
     }
 }
